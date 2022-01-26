969048a4ddf4d0a9152ae10a8f986be9ff657d73
Alex32 <alex32.taem@gmail.com>
2022-01-25 20:39:50 -0600

    curso

--- curso_flutter_web/lib/main.dart
+++ curso_flutter_web/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -30,43 +32,154 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  dynamic signo = '?', resultado = 0;
+  TextEditingController num1 = TextEditingController();
+  TextEditingController num2 = TextEditingController();
+  void suma({double num1 = 0, double num2 = 0}) {
+    resultado = num1 + num2;
+    setState(() {
+      signo = '+';
+      resultado = resultado;
+    });
+  }
+
+  void resta({double num1 = 0, double num2 = 0}) {
+    setState(() {
+      signo = '-';
+    });
+  }
+
+  void division({double num1 = 0, double num2 = 0}) {
+    setState(() {
+      signo = '/';
+    });
+  }
+
+  void multiplicacion({double num1 = 0, double num2 = 0}) {
+    setState(() {
+      signo = '*';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Row(
+            child: Column(
       children: <Widget>[
         Text(
-          'Hola Mundo',
-          style: TextStyle(fontSize: 36.8, color: Colors.cyan[500]),
+          '''Elige la operación: 
+          ''',
+          style: GoogleFonts.abel(fontSize: 38.5),
         ),
-        Text(
-          '''Estamos probando Flutter''',
-          style: TextStyle(
-              fontSize: 32,
-              backgroundColor: Colors.deepPurple[400],
-              decorationColor: Colors.blue,
-              fontWeight: FontWeight.bold),
-          maxLines: 1,
+
+        SizedBox(
+          child: Row(
+            children: [
+              SizedBox(
+                child: TextField(
+                  textInputAction: TextInputAction.done,
+                  style: const TextStyle(),
+                  controller: num1,
+                ),
+                width: 50.0,
+                height: 20.0,
+              ),
+              Text('  $signo'),
+              SizedBox(
+                child: TextField(
+                  textInputAction: TextInputAction.done,
+                  style: TextStyle(),
+                  controller: num2,
+                ),
+                width: 50.0,
+                height: 20.0,
+              ),
+              Text(
+                '''      =
+              ''',
+                style: GoogleFonts.adamina(fontSize: 30),
+              ),
+              SizedBox(
+                child: TextField(
+                  textInputAction: TextInputAction.done,
+                  style: const TextStyle(),
+                  decoration: InputDecoration(label: Text('$resultado')),
+                ),
+                width: 50.0,
+                height: 20.0,
+              ),
+            ],
+          ),
+          width: 300,
         ),
-        Column(
-          children: <Widget>[
-            Text('Flutter es genial',
-                style: GoogleFonts.lato(
-                    textStyle: TextStyle(
-                        color: Colors.blue[600],
-                        fontSize: 29,
-                        letterSpacing: .5,
-                        fontWeight: FontWeight.w900))),
-            Text('Flutter es genial',
-                style: GoogleFonts.lato(
-                    textStyle: TextStyle(
-                        color: Colors.blue[600],
-                        fontSize: 29,
-                        letterSpacing: .5,
-                        fontWeight: FontWeight.w900))),
-          ],
+        SizedBox(
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                  onPressed: () {
+                    try {
+                      dynamic dato1, dato2;
+                      dato1 = double.tryParse(num1.text);
+                      dato2 = double.tryParse(num2.text);
+                      suma(num1: dato1, num2: dato2);
+                    } catch (e) {
+                      print(num1.text);
+                      print(num2.text);
+                      print(e.toString());
+                    }
+                  },
+                  icon: const Icon(Icons.add)),
+              IconButton(
+                  onPressed: () {
+                    resta();
+                  },
+                  icon: const Icon(Icons.subscript_rounded)),
+              IconButton(
+                  onPressed: () {
+                    multiplicacion();
+                  },
+                  icon: const Icon(Icons.subscript)),
+              IconButton(
+                  onPressed: () {
+                    division();
+                  },
+                  icon: const Icon(Icons.drive_eta)),
+            ],
+          ),
+          width: 200,
         )
+        // Text(
+        //   'Hola Mundo',
+        //   style: TextStyle(fontSize: 36.8, color: Colors.cyan[500]),
+        // ),
+        // Text(
+        //   '''Estamos probando Flutter''',
+        //   style: TextStyle(
+        //       fontSize: 32,
+        //       backgroundColor: Colors.deepPurple[400],
+        //       decorationColor: Colors.blue,
+        //       fontWeight: FontWeight.bold),
+        //   maxLines: 1,
+        // ),
+        // Column(
+        //   children: <Widget>[
+        //     Text('Flutter es genial',
+        //         style: GoogleFonts.lato(
+        //             textStyle: TextStyle(
+        //                 color: Colors.blue[600],
+        //                 fontSize: 29,
+        //                 letterSpacing: .5,
+        //                 fontWeight: FontWeight.w900))),
+        //     Text('Flutter es genial',
+        //         style: GoogleFonts.lato(
+        //             textStyle: TextStyle(
+        //                 color: Colors.blue[600],
+        //                 fontSize: 29,
+        //                 letterSpacing: .5,
+        //                 fontWeight: FontWeight.w900))),
+        //   ],
+        // )
       ],
     )));
   }
